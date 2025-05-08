5ea4d145387c7bd0fa88fbf1d95784cf536f4574
Sensei-Snow <44tutur@gmail.com>
2025-05-08 20:04:55 +0200

    test

--- init_db.py
+++ init_db.py
@@ -1,37 +1,38 @@
 import json
 import os
+from werkzeug.security import generate_password_hash
 
 USERS_FILE = "users.json"
 
 def init_db():
-        # suppression des tables
-        with open(USERS_FILE, "w") as f:
-            json.dump({"codes": []}, f, indent=4)
+    # suppression des tables
+    with open(USERS_FILE, "w") as f:
+        json.dump({"codes": []}, f, indent=4)
         
-        print("Ajout de l'utilisateur admin {root}")
-        print("Adding the admin user {root}")
-        print("Veuillez entrer le mot de passe de l'utilisateur admin {root}")
-        print("Please enter the password of the admin user {root}")
-        password = input()
-        # ajout de l'utilisateur admin
-        create_user_access(password, "root")
+    print("Ajout de l'utilisateur admin {root}")
+    print("Adding the admin user {root}")
+    print("Veuillez entrer le mot de passe de l'utilisateur admin {root}")
+    print("Please enter the password of the admin user {root}")
+    password = input()
+    # ajout de l'utilisateur admin
+    create_user_access(password, "root")
 
-        print("Ajout d'un utilisateur de base (user)")
-        print("Adding a basic user (user)")
-        print("Veuillez entrer le nom d'utilisateur de l'utilisateur de base (user)")
-        print("Please enter the username of the user 'user'")
-        username = input()
-        print("Veuillez entrer le mot de passe de l'utilisateur de base (user)")
-        print("Please enter the password of the user 'user'")
-        password = input()
-        # hash du mot de passe
-        password = generate_password_hash(password)
-        # ajout de l'utilisateur de base
-        user = User(username=username, password=password, role="user")
-        db.session.add(user)
+    print("Ajout d'un utilisateur de base (user)")
+    print("Adding a basic user (user)")
+    print("Veuillez entrer le nom d'utilisateur de l'utilisateur de base (user)")
+    print("Please enter the username of the user 'user'")
+    username = input()
+    print("Veuillez entrer le mot de passe de l'utilisateur de base (user)")
+    print("Please enter the password of the user 'user'")
+    password = input()
+    # hash du mot de passe
+    password = generate_password_hash(password)
+    # ajout de l'utilisateur de base
+    user = User(username=username, password=password, role="user")
+    db.session.add(user)
         
-        db.session.commit()
-        print("Base de données initialisée avec succès et utilisateurs ajoutés.")
+    db.session.commit()
+    print("Base de données initialisée avec succès et utilisateurs ajoutés.")
 
 def create_user_access(code, role):
     data = {"codes": []}
