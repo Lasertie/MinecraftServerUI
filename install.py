36034a313f82d0773f048bd24e71f95264eb8eb2
Lasertie Zyglonk <zyglonk@proton.me>
2025-05-08 19:32:00 +0200

    sdf

--- install.py
+++ install.py
@@ -5,11 +5,11 @@ import subprocess
 def install():
     # Création de l'environnement virtuel si inexistant
     if input("Créer dans un environement virtuel ? [O(Oui)/n(Non)]") == "O" :
-            if not os.path.exists("venv"):
-                print("Création de l'environnement virtuel...")
-                subprocess.run(["python3", "-m", "venv", "venv"])
-                # Chemin vers pip dans l'environnement virtuel
-                pip_path = "./venv/bin/pip"
+        if not os.path.exists("venv"):
+            print("Création de l'environnement virtuel...")
+            subprocess.run(["python3", "-m", "venv", "venv"])
+            # Chemin vers pip dans l'environnement virtuel
+            pip_path = "./venv/bin/pip"
     else:
         print("Ok, les dépendances seront installés globalement")
         pip_path = 'pip'
