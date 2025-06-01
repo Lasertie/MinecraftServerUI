c3836a492bd77b08bd62ab18f5ea0e023b20a382
Lasertie Zyglonk <zyglonk@proton.me>
2025-06-01 11:43:58 +0200

    suite

--- new_app.py
+++ new_app.py
@@ -16,78 +16,24 @@ from functools import wraps
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from Utils.Users import User
+
 app = Flask(__name__)
-app.secret_key = "b'\x84\t\x8c\x94\xdc\x1a\x8bv\x18Ac\xaf\xf4*\xeeDu\x9e\xe4y\x02\x085a'" #os.urandom(24) # On enleve la génération de clé secrete pour les tests
+app.secret_key = "b'\x84\t\x8c\x94\xdc\x1a\x8bv\x18Ac\xaf\xf4*\xeeDu\x9e\xe4y\x02\x085a'" #os.urandom(24) # On enleve la génération de clé secrete pour les tests car sinon il faut se relog a chaque essaie
 # print(app.secret_key)
 
 # ------------------------------------------ JSON file paths ---------------------------------- #
-JSON_DIR = os.path.abspath(os.path.dirname(__file__))
-USERS_FILE = os.path.join(JSON_DIR, 'users.json')
-SERVERS_FILE = os.path.join(JSON_DIR, 'servers.json')
+JSON_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'json'))
+CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Config'))
+USERS_FILE = os.path.join(CONFIG_DIR, 'users.json')
+SERVERS_FILE = os.path.join(CONFIG_DIR, 'servers.json')
 VERSIONS_FILE = os.path.join(JSON_DIR, 'versions.json')
 COMMANDS_FILE = os.path.join(JSON_DIR, 'commands.json')
-SETTINGS_FILE = os.path.join(JSON_DIR, 'settings.json')
-with open('settings.json') as f:
+SETTINGS_FILE = os.path.join(CONFIG_DIR, 'settings.json')
+with open(SETTINGS_FILE) as f:
     JAVA_PATH = json.load(f)
     JAVA_PATH = JAVA_PATH['java_path'] #"/usr/bin/java"
 
-# --- User management ---
-class User(UserMixin):
-    def __init__(self, id, username, password_hash, role):
-        self.id = str(id)
-        self.username = username
-        self.password_hash = password_hash
-        self.role = role
-
-    @staticmethod
-    def load_all():
-        if not os.path.exists(USERS_FILE):
-            return {}
-        with open(USERS_FILE, 'r') as f:
-            return json.load(f)
-
-    @staticmethod
-    def save_all(users):
-        with open(USERS_FILE, 'w') as f:
-            json.dump(users, f, indent=2)
-
-    @classmethod
-    def get(cls, username):
-        users = cls.load_all()
-        for uid, u in users.items():
-            if u['username'] == username:
-                return cls(uid, u['username'], u['password_hash'], u['role'])
-        return None
-
-    @classmethod
-    def get_by_id(cls, id):
-        users = cls.load_all()
-        u = users.get(str(id))
-        if u:
-            return cls(id, u['username'], u['password_hash'], u['role'])
-        return None
-
-    @classmethod
-    def create(cls, username, password, role):
-        users = cls.load_all()
-        # assign new id
-        new_id = max([int(i) for i in users.keys()] + [0]) + 1
-        users[str(new_id)] = {
-            'username': username,
-            'password_hash': generate_password_hash(password),
-            'role': role
-        }
-        cls.save_all(users)
-        return cls.get(username)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def delete(self):
-        users = User.load_all()
-        if self.id in users:
-            del users[self.id]
-            User.save_all(users)
 
 # ------------------------------------------------- Flask-Login setup ---------------------------------------------------#
 login_manager = LoginManager()
